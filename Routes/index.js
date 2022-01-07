3468c2178f4d43612196e6a7b909dcc7f4023142
Gaurav mehra <gauravmehra1298@gmail.com>
2022-01-07 18:42:53 +0530

    a

--- Routes/index.js
+++ Routes/index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const router=express.Router();
 
 //if url has product  
+router.use('/',function(req,res){
+    res.render('<h1>Working</h1>');
+})
 router.use('/products',require('./products'));
 
 module.exports=router;
