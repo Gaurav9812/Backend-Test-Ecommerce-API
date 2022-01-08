b8f451968faaa33ffcf5e8dd4b16baecf2cdb18d
Gaurav mehra <gauravmehra1298@gmail.com>
2022-01-08 17:24:02 +0530

    Final commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb://localhost/ecommerce_app');
+// mongoose.connect('mongodb://localhost/ecommerce_app');
+
+mongoose.connect('mongodb+srv://mehra213:Zd6UGyhgNDvopnHl@cluster0.pooba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
 
 const db=mongoose.connection;
